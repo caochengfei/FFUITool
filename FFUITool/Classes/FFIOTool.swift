f173f6f122c05b93ce0f151e509255bb599ba9bf
cofey <cchengfei@outlook.com>
2022-08-26 14:43:46 +0800

    add

--- FFUITool/Classes/FFIOTool.swift
+++ FFUITool/Classes/FFIOTool.swift
@@ -7,13 +7,63 @@
 
 import Foundation
 
+public enum FFIOToolDirectory: String {
+    case document
+    case library
+    case caches
+    case tmp
+}
+
 public class FFIOTool : NSObject {
+    
     public static var documentPath: String {
-        return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        return documentUrl.path
     }
     
-    public static var libraryPath: String {
-        return NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).last!
+    public static var documentUrl: URL {
+        let documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return documentUrl
+    }
+    
+    public static var LibraryPath: String {
+        return libraryUrl.path
+    }
+    
+    public static var libraryUrl: URL {
+        let libraryUrl = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first!
+        return libraryUrl
+    }
+    
+    public static var cacheUrl: URL {
+        let cacheUrl = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        return cacheUrl
+    }
+    
+    public static var cachePath: String {
+        return cacheUrl.path
+    }
+
+    
+    /// 创建文件夹
+    /// - Parameters:
+    ///   - directoryName: 文件夹名字
+    ///   - mainDirectory: 主目录
+    ///   - skipBackup: 是否阻止icloud备份
+    /// - Returns: 完整路径URL
+    public static func createDirectory(directoryName: String, mainDirectory: URL? = nil, skipBackup: Bool = true) -> URL {
+        var url: URL! = mainDirectory == nil ? cacheUrl.appendingPathComponent(directoryName) : mainDirectory?.appendingPathComponent(directoryName)
+        
+        var isDirectory = ObjCBool.init(false)
+        let isExist = FileManager.default.fileExists(atPath: url.path, isDirectory: &isDirectory)
+        if !isExist {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+                if skipBackup { addSkipBackupAttributeToItemAtUrl(url: &url) }
+            } catch {
+                ffPrint("createDirectory: \(directoryName) error\(error)")
+            }
+        }
+        return url
     }
     
     public static func copyItem(fromPath: String, toPath: String) {
@@ -27,6 +77,33 @@ public class FFIOTool : NSObject {
             try FileManager.default.copyItem(at: fromUrl, to: toUrl)
         } catch {
                 
+        }
+    }
+    
+    /// 清空文件夹下的所有文件
+    /// - Parameter url: 文件夹路径
+    public static func clearDirectory(url: URL) {
+        guard let files = FileManager.default.subpaths(atPath: url.path) else {return}
+        for file in files {
+            try? FileManager.default.removeItem(atPath: url.path + "/\(file)")
+        }
+    }
+    
+    /// 删除文件或者文件夹
+    /// - Parameter url: 文件/文件夹URL
+    public static func removeFile(url: URL) {
+        try? FileManager.default.removeItem(at: url)
+    }
+    
+    /// 设置排序icloud云端备份的文件夹
+    /// - Parameter url: 文件夹路径
+    public static func addSkipBackupAttributeToItemAtUrl(url: inout URL) {
+        do {
+            var resourceValues = URLResourceValues()
+            resourceValues.isExcludedFromBackup = true
+            try url.setResourceValues(resourceValues)
+        } catch  {
+            ffPrint(error)
         }
     }
 }
