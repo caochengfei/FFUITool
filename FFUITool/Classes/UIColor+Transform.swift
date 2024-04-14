c9d84fd8d3b0400795e64ab2cdae9da961e82669
cofey <cchengfei@outlook.com>
2024-04-14 08:41:22 +0800

    修改 color 支持 p3

--- FFUITool/Classes/UIColor+Transform.swift
+++ FFUITool/Classes/UIColor+Transform.swift
@@ -11,13 +11,16 @@ import SwiftUI
 extension String {
     /// 十六进制颜色转换为UIColor
     public var toRGB: UIColor {
-        return uicolor(alpha: 1.0)
+        return uicolor(alpha: 1.0, p3: false)
     }
     
+    public var toP3: UIColor {
+        return uicolor(alpha: 1.0, p3: true)
+    }
     /// 十六进制颜色转换为UIColor
     /// - Parameter alpha: 透明度
     /// - Returns: UIColor
-    public func uicolor(alpha: CGFloat? = 1.0) -> UIColor {
+    public func uicolor(alpha: CGFloat? = 1.0, p3: Bool = true) -> UIColor {
         var red: UInt64 = 0, green: UInt64 = 0, blue: UInt64 = 0, _alpha: UInt64 = 0
         var hex = self
         // 去掉前缀
@@ -42,7 +45,10 @@ extension String {
             return UIColor(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: CGFloat(_alpha) / 255.0)
         }
         
-        return UIColor(displayP3Red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha ?? 1.0)
+        if p3 == true {
+            return UIColor(displayP3Red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha ?? 1.0)
+        }
+        return UIColor(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha ?? 1.0)
     }
     
     @available(iOS 13.0, *)
@@ -51,8 +57,13 @@ extension String {
     }
     
     @available(iOS 13.0, *)
-    public func color(alpha: CGFloat = 1.0) -> Color {
-        return Color(self.uicolor(alpha: alpha))
+    public var toP3Color: Color {
+        return Color(self.toRGB)
+    }
+    
+    @available(iOS 13.0, *)
+    public func color(alpha: CGFloat = 1.0, p3: Bool = true) -> Color {
+        return Color(self.uicolor(alpha: alpha, p3: p3))
     }
 }
 
