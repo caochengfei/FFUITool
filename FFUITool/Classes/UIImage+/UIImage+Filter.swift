c7cc127d305dbacc0e477a3d2ab0f30bc85a83a8
cofey <cchengfei@outlook.com>
2024-09-06 14:49:30 +0800

    add

--- FFUITool/Classes/UIImage+/UIImage+Filter.swift
+++ FFUITool/Classes/UIImage+/UIImage+Filter.swift
@@ -34,7 +34,7 @@ extension UIImage {
     
     
     /// 高斯模糊图
-    public func blurImage(radius: CGFloat = 10.0) -> UIImage? {
+    public func gaussianBlur(radius: CGFloat = 10.0) -> UIImage? {
         guard let inputImage = CIImage(image: self) else {
             return nil
         }
@@ -51,6 +51,30 @@ extension UIImage {
             return nil
         }
         
+        let content = CIContext()
+        if let cgImg = content.createCGImage(outputImage, from: inputImage.extent) {
+            return UIImage(cgImage: cgImg)
+        }
+        return nil
+    }
+    
+    /// 高斯模糊图
+    public func mediaBlur(radius: CGFloat = 3) -> UIImage? {
+        guard let inputImage = CIImage(image: self) else {
+            return nil
+        }
+                
+        let affineClampFilter = CIFilter(name: "CIAffineClamp")
+        affineClampFilter?.setDefaults()
+        affineClampFilter?.setValue(inputImage, forKey: kCIInputImageKey)
+        
+        let filter = CIFilter(name: "CIMedianFilter")
+        filter?.setDefaults()
+        filter?.setValue(affineClampFilter?.outputImage, forKey: kCIInputImageKey)
+//        filter?.setValue(radius, forKey: kCIInputRadiusKey)
+        guard let outputImage = filter?.outputImage else {
+            return nil
+        }
         let content = CIContext()
         if let cgImg = content.createCGImage(outputImage, from: inputImage.extent) {
             return UIImage(cgImage: cgImg)
